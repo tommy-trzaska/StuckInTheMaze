dccdddcbc36643dd573f38d5a1ff0d11f2287cdc
Tomasz Trzaska <tommy.trzaska@gmail.com>
2016-02-27 16:41:48 +0100

    Limited minimap

---
+++ SitM/Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MinimapScript : MonoBehaviour {
+
+    public GameObject minimap;
+    public Text timerText;
+
+    private float timeLeft = 10;
+
+    void Awake ()
+    {
+        minimap.SetActive(false);
+    }
+
+    void Update ()
+    {
+        timerText.text = timeLeft.ToString("F1");
+
+        if(Input.GetMouseButtonDown(0) && timeLeft > 0)
+        {
+            minimap.SetActive(true);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            timeLeft -= Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonUp(0) || timeLeft <= 0)
+        {
+            minimap.SetActive(false);
+        }
+    }
+}
